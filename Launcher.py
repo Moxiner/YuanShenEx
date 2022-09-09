6cdf121fc982cd152ccf3aa322fc63f1c5d98070
Moxiner <2665348076@qq.com>
2022-09-10 02:16:28 +0800

    📏 更新 其他

--- Launcher.py
+++ Launcher.py
@@ -1,7 +1,3 @@
-from msilib.schema import Error
-from re import A
-from typing import Any
-from xml.etree.ElementTree import VERSION
 from PIL import Image ,ImageTk
 from tkinter import END, messagebox , font
 import tkinter as tk 
@@ -27,6 +23,7 @@ def readConfig():
 def mihoyo():
     '''将配置文件改成官服配置并启动游戏'''
     try:
+        global NOTE
         config = configparser.ConfigParser()
         config.read("Config.ini") 
         config.set("General","channel","1")
@@ -40,12 +37,11 @@ def mihoyo():
     except:
         messagebox.showerror(title="找不到配置文件" , message="找不到配置文件\n请将启动器移动至游戏目录\n并确保目录里有 Config.ini 文件")
 
-    
-
 
 def bilibili():
     '''将配置文件改成B服配置并启动游戏'''
     try:
+        global NOTE
         config = configparser.ConfigParser()
         config.read("Config.ini")
         config.set("General","channel","14")
@@ -53,18 +49,18 @@ def bilibili():
         config.set("General","sub_channel","0")
         config.write(open("Config.ini", "w"))
         try:
-            os.system("start yuanshen.exe")     
+            os.system("start yuanshen.exe")
+                 
         except:
             messagebox.showerror(title="找不到配置文件" , message="找不到游戏本体\n请将启动器移动至游戏目录\n并确保目录里有 yuanshen.exe 文件")
     except:
         messagebox.showerror(title="找不到配置文件" , message="找不到配置文件\n请将启动器移动至游戏目录\n并确保目录里有 Config.ini 文件")
     
     
-
-
 def main():
     '''主函数'''
     # 创造窗口
+    global Note
     global Window
     Window = tk.Tk()
     Window.title(f"原神启动器 {VERSION}")
@@ -82,7 +78,6 @@ def main():
     except:
         messagebox.showerror(title="缺少文件" , message="缺少资源文件 src\\background.png \n请重新解压压缩包内所有文件！")
     canvas.create_image(640, 360, image=bg_img)
-
     # 绘制控件
     mihoyo_button = tk.Button(Window_frame , text="启动官服",width=23 , command=mihoyo ,height=40 ,background="#FFCB20" ,bd=0 ,activebackground="#d2a617" ,font=("微软雅黑",12,"bold"  ) , fg="#89601A")
     bilibili_button = tk.Button(Window_frame , text="启动B服",width=23 ,command=bilibili ,height=40 , background="#FFCB20" , bd=0 ,activebackground="#d2a617", font=("微软雅黑",12,"bold" ) , fg="#89601A")
@@ -90,16 +85,16 @@ def main():
     mihoyo_button.pack(side="left")
     bilibili_button.pack(side="left")
     canvas.create_window(1000, 610, width=470, height=60 , window = Window_frame)
-    canvas.create_text(1260, 710 , text= NOTE ,anchor="se", font=("微软雅黑",10, ) , fill="white") 
+    Note = canvas.create_text(1260, 710 , text= NOTE ,anchor="se", font=("微软雅黑",10, ) , fill="white") 
     canvas.pack()
 
 
     # 绘制窗口属性
+    Window.resizable(width=False, height=False)
     x_cordinate = int((Window.winfo_screenwidth() / 2) - (1280 / 2))
     y_cordinate = int((Window.winfo_screenheight() / 2) - (720 / 2))
     Window.geometry("1280x720+{}+{}".format(x_cordinate, y_cordinate-20))
     Window.mainloop()
-
 
 
 if __name__ == "__main__":
