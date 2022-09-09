894391a7ce559877d7a44e2ee9833b8630ae68bb
Moxiner <2665348076@qq.com>
2022-09-10 01:08:13 +0800

    📏 更新 底部文字

--- Launcher.py
+++ Launcher.py
@@ -1,3 +1,6 @@
+from msilib.schema import Error
+from re import A
+from typing import Any
 from xml.etree.ElementTree import VERSION
 from PIL import Image ,ImageTk
 from tkinter import END, messagebox , font
@@ -5,15 +8,24 @@ import tkinter as tk
 from PIL import Image, ImageTk
 import os
 import configparser
-try:
-    config = configparser.ConfigParser()
-    config.read("Config.ini")
-    VERSION = "1.0.0"
-    GAME_VERSON = config.get("General","game_version" ) 
-except:
-    messagebox.showerror(title="找不到配置文件" , message="找不到配置文件\n请将启动器移动至游戏目录\n并确保目录里有 Config.ini")
 
+VERSION = "1.0.1"
+AUTHOR = "Moxiner"
+
+def readConfig():
+    '''读取配置文件'''
+    try:
+        global NOTE
+        global VERSION
+        config = configparser.ConfigParser()
+        config.read("Config.ini")
+        GAME_VERSON = config.get("General","game_version" ) 
+        NOTE = f"欢迎使用原神启动器EX    游戏版本 {GAME_VERSON}    作者 {AUTHOR}"
+    except:
+        messagebox.showerror(title="找不到配置文件" , message="找不到配置文件\n请将启动器移动至游戏目录\n并确保目录里有 Config.ini")
+        NOTE = "读取配置文件失败 , 未找到游戏文件"
 def mihoyo():
+    '''将配置文件改成官服配置并启动游戏'''
     try:
         config = configparser.ConfigParser()
         config.read("Config.ini") 
@@ -32,8 +44,8 @@ def mihoyo():
 
 
 def bilibili():
+    '''将配置文件改成B服配置并启动游戏'''
     try:
-        global GAME_VERSON
         config = configparser.ConfigParser()
         config.read("Config.ini")
         config.set("General","channel","14")
@@ -51,39 +63,48 @@ def bilibili():
 
 
 def main():
+    '''主函数'''
+    # 创造窗口
     global Window
     Window = tk.Tk()
-    Window.title(f"原神启动器 V{VERSION} With Game V{GAME_VERSON}")
+    Window.title(f"原神启动器 {VERSION}")
     try:
-        Window.iconbitmap("ico.ico")
+        Window.iconbitmap("./src/ico.ico")
     except:
-        pass
+        messagebox.showerror(title="缺少文件" , message="缺少资源文件 src\\ico.ico \n请重新解压压缩包内所有文件！")
     canvas = tk.Canvas(Window, width=1280,height=720,bd=0, highlightthickness=0)
     Window_frame = tk.Frame(canvas)
-    bg_load = Image.open("background.png")
-    bg_img = ImageTk.PhotoImage(bg_load)
-    
 
-
-
+    # 加载背景图片
+    try:
+        bg_load = Image.open("./src/background.png")
+        bg_img = ImageTk.PhotoImage(bg_load)
+    except:
+        messagebox.showerror(title="缺少文件" , message="缺少资源文件 src\\background.png \n请重新解压压缩包内所有文件！")
     canvas.create_image(640, 360, image=bg_img)
-    mihoyo_button = tk.Button(Window_frame , text="启动官服",width=27 , command=mihoyo ,height=40 ,background="#FFCB20" ,bd=0 ,activebackground="#d2a617")
-    bilibili_button = tk.Button(Window_frame , text="启动B服",width=27 ,command=bilibili ,height=40 , background="#FFCB20" , bd=0 ,activebackground="#d2a617")
+
+    # 绘制控件
+    mihoyo_button = tk.Button(Window_frame , text="启动官服",width=23 , command=mihoyo ,height=40 ,background="#FFCB20" ,bd=0 ,activebackground="#d2a617" ,font=("微软雅黑",12,"bold"  ) , fg="#89601A")
+    bilibili_button = tk.Button(Window_frame , text="启动B服",width=23 ,command=bilibili ,height=40 , background="#FFCB20" , bd=0 ,activebackground="#d2a617", font=("微软雅黑",12,"bold" ) , fg="#89601A")
     Window_frame.pack(side="left")
     mihoyo_button.pack(side="left")
     bilibili_button.pack(side="left")
-    canvas.create_window(1000, 610, width=380, height=60 , window = Window_frame)
+    canvas.create_window(1000, 610, width=470, height=60 , window = Window_frame)
+    canvas.create_text(1260, 710 , text= NOTE ,anchor="se", font=("微软雅黑",10, ) , fill="white") 
     canvas.pack()
 
 
-
+    # 绘制窗口属性
     x_cordinate = int((Window.winfo_screenwidth() / 2) - (1280 / 2))
     y_cordinate = int((Window.winfo_screenheight() / 2) - (720 / 2))
     Window.geometry("1280x720+{}+{}".format(x_cordinate, y_cordinate-20))
     Window.mainloop()
 
+
+
 if __name__ == "__main__":
     try:
+        readConfig()
         main()
-    except:
-        messagebox.showerror(title="致命错误" , message="找不到背景图片\n并确保目录里有 background.png 文件")
+    except Exception as Error :
+        messagebox.showerror(title="致命错误" , message= str(Error))
